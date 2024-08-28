6e666c8d4f5948a2d40919a6be2846bbb98795d6
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2024-08-28 19:19:43 -0300

    Implementando formulário

--- chat/lib/components/auth_form.dart
+++ chat/lib/components/auth_form.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:chat/components/user_image_picker.dart';
 import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({super.key});
+  final void Function(AuthFormData) onSubmit;
+  const AuthForm({super.key, required this.onSubmit});
 
   @override
   State<AuthForm> createState() => _AuthFormState();
@@ -11,9 +15,43 @@ class AuthForm extends StatefulWidget {
 class _AuthFormState extends State<AuthForm> {
   final AuthFormData _authFormData = AuthFormData();
   final _formKey = GlobalKey<FormState>();
+  final _emailFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+
+  void _handleImagePicker(File image) {
+    _authFormData.image = image;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+  }
 
   void submit() {
     final valid = _formKey.currentState?.validate() ?? false;
+    if (!valid) return;
+
+    if (_authFormData.image == null && _authFormData.isSignup) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            backgroundColor: Theme.of(context).colorScheme.error,
+            content: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Imagem não selecionada!',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            )),
+      );
+      return;
+    }
+
+    widget.onSubmit(_authFormData);
   }
 
   @override
@@ -27,23 +65,61 @@ class _AuthFormState extends State<AuthForm> {
           child: Column(
             children: [
               if (_authFormData.isSignup)
+                UserImagePicker(
+                  onImagePicker: _handleImagePicker,
+                ),
+              if (_authFormData.isSignup)
                 TextFormField(
-                  decoration: InputDecoration(label: Text('Nome')),
-                  key: ValueKey('name'),
+                  decoration: const InputDecoration(label: Text('Nome')),
+                  key: const ValueKey('name'),
                   initialValue: _authFormData.name,
                   onChanged: (value) => _authFormData.name = value,
+                  validator: (value) {
+                    final name = value ?? '';
+                    if (name.trim().length < 5) {
+                      return 'Nome deve ter no mínimo 5 caracteres!';
+                    }
+                    return null;
+                  },
+                  onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) =>
+                          FocusScope.of(context).requestFocus(_emailFocus),
                 ),
               TextFormField(
-                decoration: InputDecoration(label: Text('Email')),
-                key: ValueKey('email'),
+                decoration: const InputDecoration(label: Text('E-mail')),
+                key: const ValueKey('email'),
+                focusNode: _emailFocus,
                 initialValue: _authFormData.email,
                 onChanged: (value) => _authFormData.email = value,
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                textInputAction: TextInputAction.next,
+                validator: (value) {
+                  final email = value ?? '';
+                  if (!email.contains('@')) {
+                    return 'O e-mail informado não é valido!';
+                  }
+                  return null;
+                },
+                  onFieldSubmitted: (_) =>
+                          FocusScope.of(context).requestFocus(_passwordFocus),
               ),
               TextFormField(
-                decoration: InputDecoration(label: Text('Senha')),
-                key: ValueKey('password'),
+                decoration: const InputDecoration(label: Text('Senha')),
+                key: const ValueKey('password'),
                 initialValue: _authFormData.password,
+                focusNode: _passwordFocus,
                 onChanged: (value) => _authFormData.password = value,
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                textInputAction: TextInputAction.send,
+                onFieldSubmitted: (value) =>  submit(),
+                validator: (value) {
+                  final password = value ?? '';
+                  if (password.length < 6) {
+                    return 'A senha deve conter no mínimo 6 caracteres!';
+                  }
+                  return null;
+                },
                 obscureText: true,
                 enableSuggestions: false,
                 autocorrect: false,
--- chat/lib/pages/auth_page.dart
+++ chat/lib/pages/auth_page.dart
@@ -1,18 +1,44 @@
 import 'package:chat/components/auth_form.dart';
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool isLoding = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => isLoding = true);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      body: const Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
-      ),
-    );
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        body: Stack(
+          children: [
+            Center(
+              child: SingleChildScrollView(
+                child: AuthForm(
+                  onSubmit: _handleSubmit,
+                ),
+              ),
+            ),
+            if (isLoding)
+              Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(58, 0, 0, 0),
+                ),
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              )
+          ],
+        ));
   }
 }
